b803b522914659ab2613aa1d5b3a2a820572a7c8
Chabulsqu <mateofainalonso@gmail.com>
2023-01-23 18:42:02 -0300

    Fix Eslint warning about unused variables

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm';
 import { TileList } from '../../components/tileList/TileList';
 
